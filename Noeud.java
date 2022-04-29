051ab62eb1095ff76be5107e4e5fba29c077d63b
vvv987 <104647979+vvv987@users.noreply.github.com>
2022-04-29 21:42:38 +0100

    Update Noeud.java

--- Noeud.java
+++ Noeud.java
@@ -1,29 +1,117 @@
 
-public class Noeud {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Noeud {
 	
-	public int n ; // nombre d'objet
-	public float E=0; // l'evaluation 
-	public int idex;
-	public boolean s;
-	public int level; 
-	public boolean[] solution  = new boolean[n];
 	
-    public Noeud( int n,int idex, boolean s, int level) {
-		
-		this.n =n;
-		this.idex =idex;
-		this.level=level;
-		this.s=s;
-		solution[idex] = s;
-     }
-    
-   
-    public float E_noeud() {
-    	
-    	//la calcule ...
-    	return this.E;
+	static List<objets> Best_objets;
+    static float Best;
+    private static float P_max;
+    private List<objets> list_objets_rest;
+    private List<objets> list_objets;
+
+
+    Noeud(float P_max , List<objets> objets) {
+
+        this.list_objets_rest = new ArrayList<>(objets);
+        this.list_objets = new ArrayList<>();
+        this.P_max = P_max;
+// this
+        la_separation();
+    }
+
+ 
+    private void la_separation() {
+
+    	Noeud  right , left;
+
+        if (superieure() > Best && !this.list_objets_rest.isEmpty() && nextobjet() ) {
+        	
+        	left = new Noeud(this.list_objets,this.list_objets_rest,  true);
+        	right = new Noeud(this.list_objets,this.list_objets_rest,  false);
+
+        } 
+        else if (superieure() > Best && !this.list_objets_rest.isEmpty() && !nextobjet() ) {
+        	
+        	left = new Noeud( this.list_objets,this.list_objets_rest, true);
+        	right = null;
+
+        } 
+        else  left = right = null;
+         
+    }
+
+
+    private Noeud(List<objets> list_objets, List<objets> list_objets_rest,  boolean left) {
+
+         
+        this.list_objets_rest = new ArrayList<>(list_objets_rest);
+        this.list_objets = new ArrayList<>(list_objets);
+
+
+        if (left) {
+        	
+             this.list_objets_rest.remove(this.list_objets_rest.get(0));
+        } 
+        else {
+
+            this.list_objets.add(this.list_objets_rest.get(0));
+            this.list_objets_rest.remove(this.list_objets_rest.get(0));
+        }
+
+        // la mis a jour :
+        mettre_a_jrs();
+
+        la_separation();
     }
     
+    
+    // la borne superieure :
+    private float superieure() {
+    	
+        float b = 0;
+        
+        for (objets obj : list_objets) {
+            b = b + obj.getUtulite();
+        }
+
+        for (objets obj : list_objets_rest) {
+            b = b + obj.getUtulite();
+        }
+
+        return b;
+    }
+
+    private void mettre_a_jrs() {
+        float bst = 0;
+
+        if (this.list_objets != null) {
+            for (objets obj : this.list_objets) {
+                bst = bst +  obj.getUtulite();
+            }
+        }
  
+        if (Best< bst) {
+        	Best = bst;
+        	Best_objets = new ArrayList<>(list_objets);
+        }
+    }
+
+
+    private boolean nextobjet() {
+    	
+        float next = this.list_objets_rest.get(0).getPoids(); 
+        
+        if (!this.list_objets.isEmpty()) {
+        	
+            for (objets obj : list_objets) {
+            	next = next + obj.getPoids();
+            }
+        }
+        return (next <= P_max);
+    }
+    
 
 }
