5cd9023821a194002898acbd4c82cf3caccf71c7
vvv987 <104647979+vvv987@users.noreply.github.com>
2022-04-29 21:43:35 +0100

    Update objets.java

--- objets.java
+++ objets.java
@@ -1,17 +1,51 @@
+public class objets implements Comparable<objets> {
 
-public class objets {
-	
-	public float poid ; //poid des objets
-	public float utilite ; //l'utilite des objets
-	public String name;
-	public float rapport;
+    public float poids;
+    public float utulite;
+    public int indice;
+    public float rapport;
+    public objets(float poids, float utulite,int indice) {
+    	
+        this.poids = poids;
+        this.utulite = utulite;
+        this.indice  = indice; 
+        this.rapport = utulite / poids;
+       
+    }
 
-	public objets(int poid, int utilite, String name) {
-		
-	this.name =name;
-    	this.poid = poid;
-    	this.utilite = utilite;
-    	this.rapport = this.utilite / this.poid ;
+    void setPoids(float poids) {
+        this.poids = poids;
+    }
+    
+    //les getter :
+    float getPoids() {
+        return poids;
+    }
+
+    float getUtulite() {
+        return utulite;
+    }
+
+    int getIndice(){
+	    return indice;
+	  }
+    
+    float getRapport() {
+        return rapport;
+    }
+     
+    public String affiche_x() {
+         
+         return " x" + this.indice+" = 1" ;
      }
-	
+
+    public int compareTo(objets obj) {
+ 
+        if (getRapport() - obj.getRapport() > 0) return -1;
+        else if (getRapport() - obj.getRapport() < 0) return 1;
+        else  return 0;
+        
+    }
+
+   
 }
